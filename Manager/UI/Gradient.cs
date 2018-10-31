7d54c80cf31a0cd727ef31cb12f0f6ce9323a0cd
yanglingyun <825067524@qq.com>
2018-10-31 11:41:12 +0800

    2018-10-31 11:41

--- Manager/UI/Gradient.cs
+++ Manager/UI/Gradient.cs
@@ -14,14 +14,23 @@ namespace Framework.UI
     /// <summary>
     /// 设置颜色的上下渐变
     /// </summary>
-    //[AddComponentMenu("Manager/UI/Gradients")]
-    [System.Obsolete("Please use Gradient Instead of Gradients")]
-    class Gradients : BaseMeshEffect
+    [AddComponentMenu("Manager/UI/Gradient")]
+    public class Gradient : BaseMeshEffect
     {
+        /// <summary>
+        /// 上边的颜色
+        /// </summary>
         [SerializeField]
         public Color32 topColor = Color.white;
+        /// <summary>
+        /// 下边的颜色
+        /// </summary>
         [SerializeField]
         public Color32 bottomColor = Color.black;
+        /// <summary>
+        /// 每个字的顶点数
+        /// </summary>
+        private int m_Everyword = 6;
 
         public override void ModifyMesh(VertexHelper vh)
         {
@@ -29,7 +38,6 @@ namespace Framework.UI
             {
                 return;
             }
-
             var vertexList = new List<UIVertex>();
             vh.GetUIVertexStream(vertexList);
             int count = vertexList.Count;
@@ -44,27 +52,46 @@ namespace Framework.UI
 
         private void ApplyGradient(List<UIVertex> vertexList, int start, int end)
         {
+            //下顶点
             float bottomY = vertexList[0].position.y;
+            //上顶点
             float topY = vertexList[0].position.y;
+            int idx = 0;
             for (int i = start; i < end; ++i)
             {
                 float y = vertexList[i].position.y;
-                if (y > topY)
-                {
-                    topY = y;
-                }
-                else if (y < bottomY)
+                if (y > bottomY)
                 {
                     bottomY = y;
                 }
+                else if (y < topY)
+                {
+                    topY = y;
+                }
+                if ((i + 1) % m_Everyword == 0)
+                {
+                    SettingEveryword(vertexList,topY, bottomY,idx);
+                    if (i != end - 1)
+                    {
+                        topY = vertexList[i + 1].position.y;
+                        bottomY = vertexList[i + 1].position.y;
+                    }
+                    idx = i + 1;
+                }
             }
-
-            float uiElementHeight = topY - bottomY;
-            for (int i = start; i < end; ++i)
+        }
+        /// <summary>
+        /// 设置每个字的颜色信息
+        /// </summary>
+        void SettingEveryword(List<UIVertex> vertexList,float topY,float bottomY,int idx)
+        {
+            float uiElementHeight = Mathf.Abs(topY - bottomY);
+            Debug.LogError(uiElementHeight);
+            for (int j = idx; j < idx + m_Everyword; j++)
             {
-                UIVertex uiVertex = vertexList[i];
-                uiVertex.color = Color32.Lerp(bottomColor, topColor, (uiVertex.position.y - bottomY) / uiElementHeight);
-                vertexList[i] = uiVertex;
+                UIVertex uiVertex = vertexList[j];
+                uiVertex.color = Color32.Lerp(bottomColor, topColor, (uiVertex.position.y - topY) / uiElementHeight);
+                vertexList[j] = uiVertex;
             }
         }
     }
--- Manager/UI/Gradients.cs
+++ Manager/UI/Gradients.cs
@@ -14,23 +14,14 @@ namespace Framework.UI
     /// <summary>
     /// 设置颜色的上下渐变
     /// </summary>
-    [AddComponentMenu("Manager/UI/Gradient")]
-    public class Gradient : BaseMeshEffect
+    //[AddComponentMenu("Manager/UI/Gradients")]
+    [System.Obsolete("Please use Gradient Instead of Gradients")]
+    class Gradients : BaseMeshEffect
     {
-        /// <summary>
-        /// 上边的颜色
-        /// </summary>
         [SerializeField]
         public Color32 topColor = Color.white;
-        /// <summary>
-        /// 下边的颜色
-        /// </summary>
         [SerializeField]
         public Color32 bottomColor = Color.black;
-        /// <summary>
-        /// 每个字的顶点数
-        /// </summary>
-        private int m_Everyword = 6;
 
         public override void ModifyMesh(VertexHelper vh)
         {
@@ -38,6 +29,7 @@ namespace Framework.UI
             {
                 return;
             }
+
             var vertexList = new List<UIVertex>();
             vh.GetUIVertexStream(vertexList);
             int count = vertexList.Count;
@@ -52,46 +44,27 @@ namespace Framework.UI
 
         private void ApplyGradient(List<UIVertex> vertexList, int start, int end)
         {
-            //下顶点
             float bottomY = vertexList[0].position.y;
-            //上顶点
             float topY = vertexList[0].position.y;
-            int idx = 0;
             for (int i = start; i < end; ++i)
             {
                 float y = vertexList[i].position.y;
-                if (y > bottomY)
-                {
-                    bottomY = y;
-                }
-                else if (y < topY)
+                if (y > topY)
                 {
                     topY = y;
                 }
-                if ((i + 1) % m_Everyword == 0)
+                else if (y < bottomY)
                 {
-                    SettingEveryword(vertexList,topY, bottomY,idx);
-                    if (i != end - 1)
-                    {
-                        topY = vertexList[i + 1].position.y;
-                        bottomY = vertexList[i + 1].position.y;
-                    }
-                    idx = i + 1;
+                    bottomY = y;
                 }
             }
-        }
-        /// <summary>
-        /// 设置每个字的颜色信息
-        /// </summary>
-        void SettingEveryword(List<UIVertex> vertexList,float topY,float bottomY,int idx)
-        {
-            float uiElementHeight = Mathf.Abs(topY - bottomY);
-            Debug.LogError(uiElementHeight);
-            for (int j = idx; j < idx + m_Everyword; j++)
+
+            float uiElementHeight = topY - bottomY;
+            for (int i = start; i < end; ++i)
             {
-                UIVertex uiVertex = vertexList[j];
-                uiVertex.color = Color32.Lerp(bottomColor, topColor, (uiVertex.position.y - topY) / uiElementHeight);
-                vertexList[j] = uiVertex;
+                UIVertex uiVertex = vertexList[i];
+                uiVertex.color = Color32.Lerp(bottomColor, topColor, (uiVertex.position.y - bottomY) / uiElementHeight);
+                vertexList[i] = uiVertex;
             }
         }
     }
