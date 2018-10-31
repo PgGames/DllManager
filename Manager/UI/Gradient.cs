695a5543f62ffe068547513d97a345d519eff034
yanglingyun <825067524@qq.com>
2018-10-31 12:07:35 +0800

    2018-10-31 12:00

--- Manager/UI/Gradient.cs
+++ Manager/UI/Gradient.cs
@@ -28,6 +28,11 @@ namespace Framework.UI
         [SerializeField]
         public Color32 bottomColor = Color.black;
         /// <summary>
+        /// 
+        /// </summary>
+        public Whole m_Whole = Whole.Every;
+
+        /// <summary>
         /// 每个字的顶点数
         /// </summary>
         private int m_Everyword = 6;
@@ -68,16 +73,23 @@ namespace Framework.UI
                 {
                     topY = y;
                 }
-                if ((i + 1) % m_Everyword == 0)
+                if (m_Whole == Whole.Every)
                 {
-                    SettingEveryword(vertexList,topY, bottomY,idx);
-                    if (i != end - 1)
+                    if ((i + 1) % m_Everyword == 0)
                     {
-                        topY = vertexList[i + 1].position.y;
-                        bottomY = vertexList[i + 1].position.y;
+                        SettingEveryword(vertexList, topY, bottomY, idx);
+                        if (i != end - 1)
+                        {
+                            topY = vertexList[i + 1].position.y;
+                            bottomY = vertexList[i + 1].position.y;
+                        }
+                        idx = i + 1;
                     }
-                    idx = i + 1;
                 }
+            }
+            if (m_Whole == Whole.Whole)
+            {
+                SettingWholeWord(vertexList, topY, bottomY, start, end);
             }
         }
         /// <summary>
@@ -92,6 +104,40 @@ namespace Framework.UI
                 uiVertex.color = Color32.Lerp(bottomColor, topColor, (uiVertex.position.y - topY) / uiElementHeight);
                 vertexList[j] = uiVertex;
             }
+        }
+        /// <summary>
+        /// 设置整体的颜色
+        /// </summary>
+        /// <param name="vertexList"></param>
+        /// <param name="topY"></param>
+        /// <param name="bottomY"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        void SettingWholeWord(List<UIVertex> vertexList, float topY, float bottomY, int start, int end)
+        {
+
+            float uiElementHeight = topY - bottomY;
+            for (int i = start; i < end; ++i)
+            {
+                UIVertex uiVertex = vertexList[i];
+                uiVertex.color = Color32.Lerp(bottomColor, topColor, (uiVertex.position.y - bottomY) / uiElementHeight);
+                vertexList[i] = uiVertex;
+            }
+        }
+
+        /// <summary>
+        /// 整体性
+        /// </summary>
+        public enum Whole
+        {
+            /// <summary>
+            /// 单个字体
+            /// </summary>
+            Every,
+            /// <summary>
+            /// 整体
+            /// </summary>
+            Whole,
         }
     }
 }
